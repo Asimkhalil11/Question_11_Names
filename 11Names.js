4064bcd06599d4e5ac966d98da97d2f860546c73
Asim Khalil <108312289+Asimkhalil11@users.noreply.github.com>
2022-10-06 10:38:34 +0500

    Add files via upload

---
+++ 11Names.js
@@ -0,0 +1,6 @@
+let myFriends = ["Mujeeb", "Shoaib", "tajammul", "Usama", "Mudassir"]
+console.log(myFriends[0])
+console.log(myFriends[1])
+console.log(myFriends[2])
+console.log(myFriends[3])
+console.log(myFriends[4])
